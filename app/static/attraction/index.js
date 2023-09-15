845955441d7213271c865c2a05b95a2ab6bcdd5a
jingxcc <michelle.xj.chen@gmail.com>
2023-09-15 17:43:45 +0800

    display attraction data

--- app/static/attraction/index.js
+++ app/static/attraction/index.js
@@ -1,32 +1,3 @@
-async function getAttractionData() {
-  let urlPathName = window.location.pathname;
-  let pathNameSegments = urlPathName.split("/");
-  let attractionId = pathNameSegments[pathNameSegments.length - 1];
-
-  // page num is over then existing data
-
-  let apiUrl = `${window.location.origin}/api/attraction/${attractionId}`;
-  try {
-    const response = await fetch(apiUrl);
-
-    if (response.ok) {
-      const result = await response.json();
-      return result;
-    } else {
-      window.location.href = window.location.origin;
-    }
-  } catch (err) {
-    console.error(`Error: ${err}`);
-  }
-  return false;
-}
-
-// add images, information in the page
-async function displayAttractionData() {
-  let attractionData = await getAttractionData();
-  console.log(attractionData);
-}
-
 // Carousel
 let currentImageIndex = 0;
 
@@ -58,5 +29,87 @@ function showCarouselImage(imageNum) {
   console.log(`show images: ${currentImageIndex}`);
 }
 
-showCarouselImage(currentImageIndex);
+// price
+const timePrices = [
+  { time: "am", price: 2000 },
+  { time: "pm", price: 2500 },
+];
+
+function changeTimePrices() {
+  const timeInputs = document.querySelector("#attractionFormTime > input");
+  console.log(timeInputs);
+}
+
+async function getAttractionData() {
+  let urlPathName = window.location.pathname;
+  let pathNameSegments = urlPathName.split("/");
+  let attractionId = pathNameSegments[pathNameSegments.length - 1];
+
+  let apiUrl = `${window.location.origin}/api/attraction/${attractionId}`;
+  try {
+    const response = await fetch(apiUrl);
+
+    if (response.ok) {
+      const result = await response.json();
+      return result;
+    } else {
+      window.location.href = window.location.origin;
+    }
+  } catch (err) {
+    console.error(`Error: ${err}`);
+  }
+  return false;
+}
+
+// add images, information in the page
+async function displayAttractionData() {
+  let attractionData = await getAttractionData();
+  console.log(attractionData);
+
+  const attractionTitle = document.getElementById("attractionTitle");
+  const attractionCategory = document.getElementById("attractionCategory");
+
+  attractionTitle.textContent = attractionData["data"][0]["attraction_name"];
+
+  if (attractionData["data"][0]["mrt"] !== null) {
+    mrtText = `at ${attractionData["data"][0]["mrt"].join("/")}`;
+  }
+
+  attractionCategory.textContent =
+    attractionData["data"][0]["category"] + mrtText;
+
+  const infoDescription = document.getElementById("infoDescription");
+  const infoAddress = document.getElementById("infoAddress");
+  const infoTransport = document.getElementById("infoTransport");
+
+  infoDescription.textContent = attractionData["data"][0]["description"];
+  infoAddress.textContent = attractionData["data"][0]["address"];
+  infoTransport.textContent = attractionData["data"][0]["transport"];
+
+  const carouselImageBlock = document.getElementById("carouselImageBlock");
+  const imageFragment = document.createDocumentFragment();
+  const carouselDotList = document.getElementById("carouselDotList");
+  const dotFragment = document.createDocumentFragment();
+
+  if (attractionData["data"][0]["images"].length > 0) {
+    attractionData["data"][0]["images"].forEach((item, idx) => {
+      const image = document.createElement("img");
+      image.classList.add("carousel__images");
+      image.setAttribute("src", item);
+      image.setAttribute("alt", "attraction image");
+
+      imageFragment.appendChild(image);
+
+      const dot = document.createElement("dot");
+      dot.classList.add("carousel__dot");
+      dotFragment.appendChild(dot);
+    });
+    carouselImageBlock.appendChild(imageFragment);
+    carouselDotList.appendChild(dotFragment);
+  }
+
+  showCarouselImage(currentImageIndex);
+}
+
 displayAttractionData();
+changeTimePrices();
